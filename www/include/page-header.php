0ad6e99fee6ca7707f699f973d59cb6fb7ebcc3c
David ibanez <David ibanez>
2016-04-28 11:03:26 +0900

    test package loading

--- www/include/page-header.php
+++ www/include/page-header.php
@@ -13,7 +13,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="shortcut icon" href="src/img/favicon.ico" type="image/x-icon">
         <link rel="stylesheet" href="src/css/style.css">
-         <script src="src/js/libs/require.js" data-main="src/js/<?php echo $currentPage; ?>.js"></script>
+        <script src="src/js/bundle.js" ></script>
+        <!-- <script src="src/js/libs/require.js" data-main="src/js/<?php echo $currentPage; ?>.js"></script> -->
         <noscript><link href="src/img/svg/fallbacks/icons.fallback.css" rel="stylesheet"></noscript>
       </head>
 
